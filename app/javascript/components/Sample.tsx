8913267ceaa047e44b71a8158f709e22ab123029
Minori411 <whmd.030@icloud.com>
2021-10-25 14:43:39 +0900

    webpacker

--- app/javascript/components/Sample.tsx
+++ app/javascript/components/Sample.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Sample: React.FC = () => {
-  return <>this is typescript sample.</>;
+  return <>this is typescript sampleminoiropapapa.</>;
 };
  
 export default Sample;
