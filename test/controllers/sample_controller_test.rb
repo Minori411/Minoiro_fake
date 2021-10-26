3ef8e247ee4433218fc405ced1cc4d0cac153b5c
Minori411 <whmd.030@icloud.com>
2021-10-26 20:59:56 +0900

    circleci

--- test/controllers/sample_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SampleControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get sample_index_url
-    assert_response :success
-  end
-end
