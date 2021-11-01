8fc4ac49fee73f6ae9a0863be0b5b6ff2743a19b
Minori411 <whmd.030@icloud.com>
2021-11-01 14:45:43 +0900

    修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  Rails.root.join('tmp', 'caching-dev.txt').exist?
+  Rails.root.join('tmp', 'caching-dev.txt').exist
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
@@ -24,11 +24,11 @@ Rails.application.configure do
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
-  else
+
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
-  end
+  
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
