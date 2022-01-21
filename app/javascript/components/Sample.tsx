0f8739dfb77a178b67f5a190ab7b8c898c4f20e2
Minori411 <whmd.030@icloud.com>
2022-01-21 10:45:34 +0900

    index.htmlに<base href=/ > を追加

--- app/javascript/components/Sample.tsx
+++ app/javascript/components/Sample.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Sample: React.FC = () => {
-  return <>this is typescript sampleminori.</>;
+  return <>this is typescript sampleminoriiiio.</>;
 };
- 
+
 export default Sample;
