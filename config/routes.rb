7b183887211119e13b098d191ec7f0747f19c56d
Minori411 <whmd.030@icloud.com>
2022-01-21 12:42:21 +0900

    deviseのビューを編集可能にした

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
+  
   get "sample", to: "sample#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
