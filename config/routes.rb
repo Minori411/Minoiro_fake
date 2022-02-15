d447f02f648eb8a2767a987e16c270423d9fa917
Minori411 <whmd.030@icloud.com>
2022-02-15 16:17:35 +0900

    新規登録　実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_scope :users do
+    get '/users', to: redirect("/users/sign_up")
+  end
+  
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'   
