289b4c4b9681fe7bf4a04212f76c3f7a411c30d1
Minori411 <whmd.030@icloud.com>
2022-02-16 15:50:11 +0900

    fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,8 +22,34 @@ def after_sign_up_path_for(resource)
   "/user/#{current_user.id}"
 end
   # POST /resource
+
 def create
+  @user = User.new(user_params)
+  if @user.save
+    logger.debug(e)
+    # 保存の成功をここで扱う。
+    flash[:success]="ユーザーを登録しました"
+    redirect_to user_path
+  else
+    render 'new'
+  end
 end
+
+begin
+rescue => e
+  logger.debug(e)
+end
+	
+
+
+
+private
+
+def user_params
+  params.require(:user).permit(:name,:email,:password,:password_confirmation)
+end
+
+
 
   # GET /resource/edit
   # def edit
