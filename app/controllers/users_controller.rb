684ec2cca258ed80347ee4abcf2e4d35314fb632
Minori411 <whmd.030@icloud.com>
2022-02-21 17:46:10 +0900

    メンターに確認してもらう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,6 @@
 class UsersController < ApplicationController
 
-    def withdrawal
-        @user = User.find(params[:id])
-        # is_deletedカラムをtrueに変更することにより削除フラグを立てる
-        @user.update(is_deleted: true)
-        reset_session
-        flash[:notice] = "退会処理を実行いたしました"
-        redirect_to root_path
-    end
+    
 
     def show
     end
@@ -30,6 +23,15 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def withdrawal
+        @user = User.find(params[:id])
+        # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+        @user.update(is_deleted: true)
+        reset_session
+        flash[:notice] = "退会処理を実行いたしました"
+        redirect_to root_path
     end
     
     private
