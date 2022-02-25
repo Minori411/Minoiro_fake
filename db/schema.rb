a3850bfbe5665bd77cc3d7c0321bc88b1fa3f1dc
Minori411 <whmd.030@icloud.com>
2022-02-25 14:05:26 +0900

    profile fix

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_24_015923) do
     t.string "remember_token"
     t.datetime "soft_destroyed_at"
     t.datetime "locked_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
