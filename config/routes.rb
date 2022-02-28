f0ccbdc34b3aa051916cf278f0a509f8ca4ab4fc
Minori411 <whmd.030@icloud.com>
2022-03-01 07:30:12 +0900

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'inquiry/index'
+  get 'inquiry/confirm'
+  get 'inquiry/thanks'
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users, :controllers => {
