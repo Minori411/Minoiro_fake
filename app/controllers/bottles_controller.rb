d617df108db6ff7e43befa57f496c246e510fc7e
Minori411 <whmd.030@icloud.com>
2022-05-24 15:50:44 +0900

    ボトルを選ぶ機能　fix

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -5,10 +5,10 @@ class BottlesController < ApplicationController
 
   def create_select_bottle
     @user_select_bottle = UserSelectBottle.new
-    @user_select_bottle.bottle_id = 1
+    @user_select_bottle.bottle_id = params[:bottle_id]
     @user_select_bottle.user_id = current_user.id
     # 何を新しく保存するか指定
-    if @user_select_bottle.save
+    if @user_select_bottle.save!
       redirect_to bottles_result_path
     end
   end
