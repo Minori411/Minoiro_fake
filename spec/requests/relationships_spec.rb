2c60d81274f06bf580f278f108e28e4127ded1c8
Minori411 <whmd.030@icloud.com>
2022-07-01 15:53:37 +0900

    test

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -1,18 +1,2 @@
 require 'rails_helper'
 
-RSpec.describe "Relationships", type: :request do
-  describe "GET /followings" do
-    it "returns http success" do
-      get "/relationships/followings"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /follwers" do
-    it "returns http success" do
-      get "/relationships/follwers"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-end
