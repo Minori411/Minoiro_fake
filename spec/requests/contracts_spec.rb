f5a48ed6af542e27592335ea4d25d1c37c885857
Minori411 <whmd.030@icloud.com>
2022-07-05 19:12:40 +0900

    rspec

--- spec/requests/contracts_spec.rb
+++ spec/requests/contracts_spec.rb
@@ -1,18 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "Contracts", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/contracts/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /shop" do
-    it "returns http success" do
-      get "/contracts/shop"
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
