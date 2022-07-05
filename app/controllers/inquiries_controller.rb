d6fe0ae1e39bf64f5493929089ddaa14f4482833
Minori411 <whmd.030@icloud.com>
2022-07-05 19:08:54 +0900

    inquiry修正

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -7,7 +7,7 @@ class InquiriesController < ApplicationController
 
   def confirm
     # 入力値のチェック
-    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :phone_number, :title, :body))
+    @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.valid?
       # OK。確認画面を表示
       render :action => 'confirm'
@@ -19,10 +19,16 @@ class InquiriesController < ApplicationController
 
   def thanks
     # メール送信
-    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email,:phone_number, :title, :body))    
+    @inquiry = Inquiry.new(inquiry_params)
     InquiriesMailer.received_email(@inquiry).deliver
 
     # 完了画面を表示
     render :action => 'thanks'
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email,:phone_number, :title, :body)
   end
 end
