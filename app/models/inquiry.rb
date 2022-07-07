98f298b6c2c3dd7fca412a2fdb54f31853160031
Minori411 <whmd.030@icloud.com>
2022-07-07 10:03:19 +0900

    presence true

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,7 +1,7 @@
 class Inquiry < ApplicationRecord
   attr_accessor :name, :email, :title, :phone_number, :body
 
-  validates :name, presence: true,
-  validates :email, presence: true,
-  validates :body, presence: true,
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :body, presence: true
 end
