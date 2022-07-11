088b620a5c42fae665335e8b8e9f694348c63a23
Minori411 <whmd.030@icloud.com>
2022-07-11 11:41:31 +0900

    BASIC認証　add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth 
+  before_action :basic_auth, if: :production?   #追加部分
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   http_basic_authenticate_with :name => ENV["BASIC_AUTH_USER"], :password => ENV["BASIC_AUTH_PASSWORD"] if Rails.env.production?
 
@@ -27,12 +28,16 @@ class ApplicationController < ActionController::Base
       redirect_to '/users/sign_in'
     end
   end
-
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #   username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
-  #   end
-  # end
+  
+  def production?   #追加部分〜
+    Rails.env.production?
+  end 
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
 
   protected
 
