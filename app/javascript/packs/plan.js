49bd4833d5feb3c801a93368cc2174d8c38cf18e
Minori411 <whmd.030@icloud.com>
2022-07-15 17:36:23 +0900

    rememeberable delete

--- app/javascript/packs/plan.js
+++ app/javascript/packs/plan.js
@@ -3,37 +3,42 @@ document.addEventListener("turbolinks:load", function () {
     $(function() {
     
       function buildField(index) {
-        const html = `<div class="boxbaseP30 plan_module flex-wrap planbox m-b-20 z-depth-1 js-addfield-block" data-index="${index}", style="padding: 30px">
-        <div class="left ">
-        <input placeholder="チャットでの相談" type="text" name="plan[plans][plan_name]" id="plan_plans_plan_name">      
-        </div>
-    
-        <div class="pricebox ">
-        <input placeholder="5,000" type="text" name="plan[plans][price]" id="plan_plans_price">
-          <div class="yenkais">
-            <span class="yenkai">円/</span>
-            <span class="kai">回</span>
-            </div>
-        </div>
-    
-    
-        <div class="items">
-            <div class="item_name flexbox">
-                <h3>相談方式</h3><span class="optional_label">任意</span>
-            </div>
-            <input id="minoiro_checkbox" as="boolean" type="checkbox" value="1" name="plan[plans][chat]">
-            <label for="plan_チャット">チャット</label>
-            <input id="minoiro_video" as="boolean" type="checkbox" value="1" name="plan[plans][video]">              
-            <label for="plan_ビデオ通話">ビデオ通話</label>
+        const html = ` <div class="boxbaseP30 plan_module js-addfield-block"  data-index: "${index}" flex-wrap planbox m-b-20 z-depth-1" style="padding: 30px">
+          <div class="left ">
+              <input placeholder="チャットでの相談" type="text" name="plan[smallplans_attributes][0][plan_name]" id="plan_smallplans_attributes_0_plan_name" />
           </div>
-    
-        <div class="m-t2 left w100p ">
-        <textarea class="w100p" placeholder="プランの内容を入力してください" name="plan[plans][plan_detail]" id="plan_plans_plan_detail"></textarea>
-      </div>
-          <div class="right m-b-20 smt_right m-t1" style="width: auto; margin-left: auto;">
-          <a class="button delete-form-btn" data-deletefiled="true" data-index="${index}">
-              <i class="fas fa-times-circle"></i> 削除
-          </a>`;
+      
+          <div class="pricebox ">
+              <input placeholder="5,000" type="text" name="plan[smallplans_attributes][0][price]" id="plan_smallplans_attributes_0_price" />
+              <div class="yenkais">
+              <span class="yenkai">円/</span>
+              <span class="kai">回</span>
+              </div>
+          </div>
+      
+      
+          <div class="items">
+              <div class="item_name flexbox">
+                  <h3>相談方式</h3><span class="optional_label">任意</span>
+              </div>
+                  <input name="plan[smallplans_attributes][0][chat]" type="hidden" value="0" autocomplete="off" /><input id="minoiro_checkbox" as="boolean" type="checkbox" value="1" name="plan[smallplans_attributes][0][chat]" />
+                  <label for="plan_smallplans_attributes_0_チャット">チャット</label>
+                  <input name="plan[smallplans_attributes][0][video]" type="hidden" value="0" autocomplete="off" /><input id="minoiro_video" as="boolean" type="checkbox" value="1" name="plan[smallplans_attributes][0][video]" />
+                  <label for="plan_smallplans_attributes_0_ビデオ通話">ビデオ通話</label>
+          </div>
+      
+          <div class="m-t2 left w100p ">
+              <textarea class="w100p" placeholder="プランの内容を入力してください" name="plan[smallplans_attributes][0][plan_detail]" id="plan_smallplans_attributes_0_plan_detail">
+      </textarea>
+          </div>
+                  <div class="right m-b-20 smt_right m-t1" style="width: auto; margin-left: auto;">
+                  <a class="button delete-form-btn" data-deletefiled="true" data-index="${index}">
+                    <i class="fas fa-times-circle"></i> 削除
+                  </a>
+              </div>
+          </div>
+          </div>
+          <div class="right m-b-20 smt_right m-t1" style="width: auto; margin-left: auto;">`;
         return html;
       }
     
