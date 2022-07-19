aa65797a91b4f4d53e96c99163d07c17b477307a
やまだのぶひこ <yamada.nobuhiko.2112@gmail.com>
2022-07-19 20:25:23 +0900

    チャットとビデオで検索可能にした

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,24 +5,24 @@ class Plan < ApplicationRecord
 
   # belongs_to :review
 
-  def self.search(keyword, price, evaluation)
+  def self.search(keyword, price, evaluation, chat, video)
     if keyword
       plan_ids = Plan.where([
                               "title like? OR body like?OR can_do like? OR status like? OR consent like?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"
                             ]).ids
-      smallplan_plan_ids = Smallplan.where(["plan_name like? OR plan_detail like? OR price like? OR video like? OR chat like?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"]).pluck(:plan_id)
+      smallplan_plan_ids = Smallplan.where(["plan_name like? OR plan_detail like? OR price like?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"]).pluck(:plan_id)
     else
       plan_ids = []
       smallplan_plan_ids = []
     end
-    smallplan = []
-    smallplan = Smallplan.where("price <= 2000") if price == "2"
+    smallplan = Smallplan.all
+    smallplan = smallplan.where("price <= 2000") if price == "2"
+    smallplan = smallplan.where("price <= 3000") if price == "3"
+    smallplan = smallplan.where("price <= 4000") if price == "4"
+    smallplan = smallplan.where("price <= 4999") if price == "5"
 
-    smallplan = Smallplan.where("price <= 3000") if price == "3"
-
-    smallplan = Smallplan.where("price <= 4000") if price == "4"
-
-    smallplan = Smallplan.where("price <= 4999") if price == "5"
+    smallplan = smallplan.where(chat: true) if chat == "1"
+    smallplan = smallplan.where(video: true) if video == "1"
 
     reviews = []
     reviews = Review.where("evaluation <= 3") if evaluation == "2"
