82943948e27c9b5fcadfc63551183ef5faf66a89
Minori411 <whmd.030@icloud.com>
2022-07-24 07:03:27 +0900

    profile機能fix

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,9 +7,10 @@ class ProfilesController < ApplicationController
     @article = @user.articles.order(created_at: :desc)
     @review = @user.reviews.order(created_at: :desc)
     @plan = @user.plans.order(created_at: :desc)
+    @user = User.find_by(id: params[:user_id])
+    @reviews = @user.reviews.order("created_at DESC")
     @avg_score = @reviews.average(:evaluation).round(1)
     @avg_score_percentage = @reviews.average(:evaluation).round(1).to_f * 100 / 5
-    @user = User.find_by(id: params[:user_id])
     @sum_total_consultants = @user.contracts.where(consultant_id: @user.id).count
     @sum_total_customers = @user.contracts.where(customer_id: @user.id).count
     @current_entry = Entry.where(user_id: current_user.id)
