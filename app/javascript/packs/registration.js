e34f2a3dd9bfb83e3634359e7b47a7fb1af584eb
Minori411 <whmd.030@icloud.com>
2022-07-23 18:10:48 +0900

    レスポンシブ

--- app/javascript/packs/registration.js
+++ app/javascript/packs/registration.js
@@ -1,27 +1,3 @@
-// document.addEventListener("turbolinks:load", function () {
-
-//     $('.icon_eye').click(event => {
-//         const input = $('#passwordForm');
-//         $('#passwordForm').get(0).type = 'text' == $('#passwordForm').get(0).type ? 'password' : 'text';
-//     })
-    
-// })
-
-// document.addEventListener("turbolinks:load", function () {
-
-//     $('.icon_eye').click(event => {
-//         const input = $('#password');
-//     $('#password').get(0).type = 'text' == $('#password').get(0).type ? 'password' : 'text';
-//     })
-// })
-
-// document.addEventListener("turbolinks:load", function () {
-
-//     $('.icon_eye').click(event => {
-//         const input = $('#passwordConfirm');
-//     $('#passwordConfirm').get(0).type = 'text' == $('#passwordConfirm').get(0).type ? 'password' : 'text';
-//     })
-// })
 document.addEventListener("turbolinks:load", function () {
     $('.icon_eye').click(function (e) {
 
